882dee7db71f167efaceb29b897b148888dcf3f4
gulin <antgulin@ya.ru>
2025-07-20 20:41:43 +0500

    upd

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 import os
 import sys
-import redis
 import base64
 import fnmatch
-import logging
-import paramiko
 import requests
 from ftplib import FTP
 from urllib.parse import urlparse
@@ -18,32 +15,6 @@ RECORD_USER = config.RECORD_USER
 RECORD_PASS = config.RECORD_PASS
 SSH_KEY = config.SSH_KEY
 RECORD_PROTOCOL = config.RECORD_PROTOCOL
-
-r = redis.Redis(host='localhost', port=6379, db=1)
-
-def setup_logger(linked_id):
-    log_dir = 'events'
-    log_filename = f'{log_dir}/{linked_id}.txt'
-    
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    
-    if r.exists(f'logger:{linked_id}'):
-        logger = logging.getLogger(linked_id)
-    else:
-        logger = logging.getLogger(linked_id)
-        logger.setLevel(logging.INFO)
-        
-        file_handler = logging.FileHandler(log_filename)
-        file_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(message)s')
-        file_handler.setFormatter(formatter)
-        
-        logger.addHandler(file_handler)
-        
-        r.set(f'logger:{linked_id}', log_filename)
-    
-    return logger
 
 
 def ftp_download(partial_file_name: str, directory: str) -> bytes:
@@ -89,9 +60,8 @@ def http_download(file_path: str) -> bytes:
         return None
     
 
-import paramiko
-
 def load_private_key(filepath, password=None):
+    import paramiko
     key_classes = [
         paramiko.RSAKey,
         paramiko.Ed25519Key,
@@ -106,6 +76,7 @@ def load_private_key(filepath, password=None):
     raise ValueError(f"Не удалось определить тип ssh-ключа: {filepath}")
 
 def download_file_sftp(remote_filepath):
+    import paramiko
     if not SSH_KEY or not os.path.exists(SSH_KEY):
         print("Private key not found")
         return None
